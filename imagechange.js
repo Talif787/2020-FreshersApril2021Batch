c50e9476b884bef2dfbc9ae3cfabb9f39e63e20b
Talif Pathan <talifpathan13@gmail.com>
2021-05-17 18:35:08 +0530

    Changed JS file after cloning

--- imagechange.js
+++ imagechange.js
@@ -1,8 +1,8 @@
 function changeImage() {
-    var bulbimage = document.getElementById('myImage');
-    if (bulbimage.src.match("bulbon")) {
-        bulbimage.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+    var bulb = document.getElementById('myImage');
+    if (bulb.src.match("bulbon")) {
+        bulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
     } else {
-        bulbimage.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+        bulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
     }
 }
